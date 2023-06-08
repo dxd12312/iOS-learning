0dd2a184ba92e5566d3267e09bce64687fd8dff9
dxd12312 <2459674360@qq.com>
2023-06-08 09:59:24 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by dxd on 2023/6/8.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
